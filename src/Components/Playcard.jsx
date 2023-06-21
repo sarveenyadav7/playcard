663e1cc27cf2e0f0f6a59085ed7b59f5f4041bb8
sarveenyadav7 <sarveenyadav7@gmail.com>
2023-06-21 16:28:07 +0530

    done

---
+++ src/Components/Playcard.jsx
@@ -0,0 +1,33 @@
+function Playcard (){
+    return(
+        <>
+            <div className="card">
+                <div className="image-card">
+                    <div className="dummy-images"></div>
+                </div>
+                <div>
+                    <h5>Sarveen Yadav</h5>
+                    <p>Frontend Developer</p>
+                </div>
+                <div className="info">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words.</div>
+                <div className="contact-icons">
+                    <div className="icons">
+                        <i class="fa fa-facebook"></i>
+                    </div>
+                    <div className="icons">
+                        <i class="fa fa-twitter" aria-hidden="true"></i>
+                    </div>
+                    <div className="icons">
+                        <i class="fa fa-github" aria-hidden="true"></i>
+                    </div>
+                     <div className="icons">
+                        <i class="fa fa-behance" aria-hidden="true"></i>
+                     </div>
+                    
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Playcard
